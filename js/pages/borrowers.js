3fa999fa9d9f7972b682795d90f27a60eccfdb7a
Ian Villanueva <ianemv@gmail.com>
2022-05-01 02:18:01 +0800

    login page

--- js/pages/borrowers.js
+++ js/pages/borrowers.js
@@ -2,7 +2,7 @@ function handleSubmitForm(e){
 	alert("i was called by submit")
 }
 
-document.querySelector("#submitbform").addEventListener("click",handleSubmitForm);
+document.querySelector("#submitbform").addEventListener("bookform",handleSubmitForm);
 
 document.querySelector("#title").addEventListener("click",function(){
 	//alert("i was focus")
---
+++ js/pages/login.js
@@ -0,0 +1,42 @@
+
+import { formToObject } from "../utils/utils.js";
+
+function processLogin(e){
+  e.preventDefault();
+  
+  let users = JSON.parse(localStorage.getItem("users"));
+  
+  if (users == null){
+    const admin = {
+      username: "admin@mail.com",
+      password: "default"
+    }
+    localStorage.setItem("users", JSON.stringify([admin]));
+    users = JSON.parse(localStorage.getItem("users"));
+  }
+
+  let data = formToObject(e.target);
+  
+  const login = {
+    password: data.password,
+    email: data.email,
+  };
+
+  const found = users.find(a => a.email == login.email )
+  if (found && found.password == login.password ){
+    window.location.replace("./admin/books")
+  }
+
+
+
+
+}
+
+
+window.addEventListener("load", function(){
+
+  if (document.querySelector("#loginform")){
+    document.querySelector("#loginform").addEventListener("submit", processLogin);
+  }
+
+}, false)
